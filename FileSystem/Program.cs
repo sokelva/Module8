3b11bfbc7fe7fdbcaaa23c632d1737245bf3c681
sokelva <sokelva@mail.ru>
2023-01-02 17:34:12 +0300

    Задание 8.1.5

--- FileSystem/Program.cs
+++ FileSystem/Program.cs
@@ -25,5 +25,19 @@ namespace FileSystem
         public string Name { get; }
         public long TotalSpace { get; }
         public long FreeSpace { get; }
+
+       
+    }
+
+    public class Folder
+    {
+        public List<string> Files { get; set; } = new List<string>();
+
+        Dictionary<string, Folder> Folders = new Dictionary<string, Folder>();
+
+        public void CreateFolder(string name)
+        {
+            Folders.Add(name, new Folder());
+        }
     }
 }
