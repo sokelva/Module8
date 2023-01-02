10c14b025a18c5bb1bb5b39de14d505bd7c1966a
sokelva <sokelva@mail.ru>
2023-01-02 17:29:23 +0300

    Задание 8.1.4

---
+++ FileSystem/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace FileSystem
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+
+    class Drive
+    {
+        public Drive(string name, long totalSpace, long freeSpace)
+        {
+            Name = name;
+            TotalSpace = totalSpace;
+            FreeSpace = freeSpace;
+        }
+
+        public string Name { get; }
+        public double TotalSpace { get; }
+        public string FreeSpace { get; }
+    }
+}
